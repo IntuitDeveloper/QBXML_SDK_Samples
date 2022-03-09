ce3bef36e4cb98aad8bd1847c432043dc745f17c
apatil14 <ankita_patil@intuit.com>
2022-03-08 22:19:54 -0800

    Update CustomerAddForm.vb

--- qbdt/vb.NET/QBFC/CustomerAdd/CustomerAddForm.vb
+++ qbdt/vb.NET/QBFC/CustomerAdd/CustomerAddForm.vb
@@ -7,7 +7,7 @@
 '
 ' Created On: 09/10/2002
 '
-' Copyright © 2002-2020 Intuit Inc. All rights reserved.
+' Copyright © 2021-2022 Intuit Inc. All rights reserved.
 ' Use is subject to the terms specified at:
 '      http://developer.intuit.com/legal/devsite_tos.html
 '
