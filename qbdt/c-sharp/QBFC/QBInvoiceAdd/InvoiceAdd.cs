b93a5195baf1aa40619d6c5ed2c4dbe3f8df1c3d
apatil14 <ankita_patil@intuit.com>
2022-03-10 20:48:11 -0800

    Upgrading QBInvoiceAdd c# to 64bit

--- qbdt/c-sharp/QBFC/QBInvoiceAdd/InvoiceAdd.cs
+++ qbdt/c-sharp/QBFC/QBInvoiceAdd/InvoiceAdd.cs
@@ -5,7 +5,7 @@ using System.ComponentModel;
 using System.Windows.Forms;
 using System.Data;
 using System.IO;
-using Interop.QBFC14;
+using Interop.QBFC15;
 
 namespace InvoiceAdd
 {
@@ -20,7 +20,7 @@ namespace InvoiceAdd
 	/// This sample also implements QBFC's automatic error recovery feature.
 	/// 
 	/// Created On: 11/19/2004
-	/// Copyright © 2004-2020 Intuit Inc. All rights reserved.
+	/// Copyright © 2021-2022 Intuit Inc. All rights reserved.
 	/// Use is subject to the terms specified at:
 	///     http://developer.intuit.com/legal/devsite_tos.html
 	///
@@ -1249,7 +1249,7 @@ namespace InvoiceAdd
 			
 
 				ICustomerRetList customerRetList = response.Detail as ICustomerRetList;
-				if (!(customerRetList.Count==0))
+				if (customerRetList != null && !(customerRetList.Count==0))
 				{
 					for (int ndx=0; ndx<=(customerRetList.Count-1); ndx++)
 					{
